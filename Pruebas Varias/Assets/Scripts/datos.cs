daded4aa346f00baabad6779b9db4aec9a231dfb
Luis Ayala <luis.g.ayala.unlam@gmail.com>
2018-10-21 22:52:14 -0300

    Versión del 21/10

---
+++ Pruebas Varias/Assets/Scripts/datos.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class datos : MonoBehaviour
+{
+    public int liderazgo = 10;
+    public int recursos = 100;
+
+    public bool eventoLiderazgo1= false;
+
+    public Text d1;
+    public Text d2;
+
+    // Use this for initialization
+    void Start(){
+        this.UpdateD1();
+        this.UpdateD2();
+
+    }
+
+    // Update is called once per frame
+    void Update(){
+        if (eventoLiderazgo1 == true){
+            this.liderazgo--;
+            this.recursos+=10;
+            this.UpdateD1();
+            this.UpdateD2();
+            eventoLiderazgo1 = false;
+        }
+    }
+
+    void UpdateD1(){
+        this.d1.text = "Liderazgo: " + this.liderazgo;
+    }
+
+    void UpdateD2(){
+        this.d2.text = "Recursos: " + this.recursos;
+    }
+}
---
+++ Pruebas Varias/Assets/Scripts/evento.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class evento : MonoBehaviour
+{
+    public string Evento = "";
+
+    void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(OnMouseUp);
+    }
+
+    void OnMouseUp()
+    {
+        SceneManager.LoadScene(Evento, LoadSceneMode.Single);
+    }
+}
